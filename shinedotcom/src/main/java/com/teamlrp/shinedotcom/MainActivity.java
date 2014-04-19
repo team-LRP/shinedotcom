85ee47aed21658dcc375e14fbfad52b6228f1224
laa-1-yay <laavanye.bahl@gmail.com>
2014-04-20 01:17:35 +0530

    audio recorder

--- shinedotcom/src/main/java/com/teamlrp/shinedotcom/MainActivity.java
+++ shinedotcom/src/main/java/com/teamlrp/shinedotcom/MainActivity.java
@@ -3,20 +3,23 @@ package com.teamlrp.shinedotcom;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-
 import java.util.Locale;
 
 
@@ -32,13 +35,21 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
 
     TextView txt1, txt2, txt3;
     EditText ed1, ed2, ed3,ed4,ed5,ed6, ed7 ;
-<<<<<<< HEAD
-=======
 
->>>>>>> FETCH_HEAD
     TextToSpeech tts;
     int i = 0;
     int counter=0;
+
+
+    Button b2;
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private MediaRecorder recorder = null;
+    private int currentFormat = 0;
+    private int output_formats[] = { MediaRecorder.OutputFormat.MPEG_4,             MediaRecorder.OutputFormat.THREE_GPP };
+    private String file_exts[] = { AUDIO_RECORDER_FILE_EXT_MP4, AUDIO_RECORDER_FILE_EXT_3GP };
+
 
 
     @Override
@@ -50,11 +61,9 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
         txt2 = (TextView) findViewById(R.id.city);
         txt3 = (TextView) findViewById(R.id.gender);
         ed1 = (EditText) findViewById(R.id.editname);
-<<<<<<< HEAD
+
         ed2 = (EditText)findViewById(R.id.editcountry);
-=======
-        ed2 = (EditText) findViewById(R.id.editcountry);
->>>>>>> FETCH_HEAD
+
         ed3 = (EditText) findViewById(R.id.editcity);
         ed4 = (EditText) findViewById(R.id.editgender);
         ed5 = (EditText) findViewById(R.id.edityears);
@@ -65,18 +74,91 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
 
         buttonSpeak = (Button)findViewById(R.id.button);
 
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        AppLog.logString("Start Recording");
+                        startRecording();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        AppLog.logString("stop Recording");
+                        stopRecording();
+                        break;
+                }
+                return false;
+            }
+        });
+
         buttonSpeak.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
 
                 speakTheText(counter);
+             }
 
 
-
-        }
+          
     });
     }
+
+
+    private String getFilename(){
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
+
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+    }
+    private void startRecording(){
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(output_formats[currentFormat]);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(getFilename());
+        recorder.setOnErrorListener(errorListener);
+        recorder.setOnInfoListener(infoListener);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private MediaRecorder.OnErrorListener errorListener = new        MediaRecorder.OnErrorListener() {
+        @Override
+        public void onError(MediaRecorder mr, int what, int extra) {
+            AppLog.logString("Error: " + what + ", " + extra);
+        }
+    };
+
+    private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
+        @Override
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+            AppLog.logString("Warning: " + what + ", " + extra);
+        }
+    };
+    private void stopRecording(){
+        if(null != recorder){
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+
+            recorder = null;
+        }
+    }
+
 
 
     @Override
@@ -124,13 +206,11 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-<<<<<<< HEAD
+
 
         EditText arr[] = {ed1,ed2, ed3,ed4,ed5,ed6, ed7};
 
-=======
-        EditText arr[] = { ed1, ed2, ed3,ed4,ed5,ed6, ed7} ;
->>>>>>> FETCH_HEAD
+
         switch (requestCode) {
             case RESULT_SPEECH: {
                 if (resultCode == RESULT_OK) {
@@ -174,11 +254,7 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
             try {
                 startActivityForResult(intent, RESULT_SPEECH);
-<<<<<<< HEAD
 
-=======
-                
->>>>>>> FETCH_HEAD
             } catch (ActivityNotFoundException e) {
                 Toast t = Toast.makeText(getApplicationContext(), "Oops major fail", Toast.LENGTH_SHORT);
                 t.show();
@@ -203,4 +279,5 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
 
 
 }
+
 
