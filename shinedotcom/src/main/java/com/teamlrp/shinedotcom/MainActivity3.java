484ffe099c4ca7d92b03896ea80040f4e97522ae
laa-1-yay <laavanye.bahl@gmail.com>
2014-04-20 16:50:27 +0530

    loatsa changesssss

--- shinedotcom/src/main/java/com/teamlrp/shinedotcom/MainActivity3.java
+++ shinedotcom/src/main/java/com/teamlrp/shinedotcom/MainActivity3.java
@@ -1,74 +1,119 @@
 package com.teamlrp.shinedotcom;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.Locale;
-
-
-public class MainActivity3 extends Activity implements TextToSpeech.OnInitListener {
-
-    protected static final int RESULT_SPEECH = 1;
+import java.io.File;
+import java.io.IOException;
 
 
-    TextToSpeech tts;
+public class MainActivity3 extends Activity {
+
+
+    Button buttonstart;
+    Button buttonstop;
+    Button Preview ;
+
+
+
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    private static final String AUDIO_RECORDER_FILE_EXT_MP3 = ".mp3";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private MediaRecorder recorder = null;
+    private int currentFormat = 0;
+    private int output_formats[] = {MediaRecorder.OutputFormat.MPEG_4, MediaRecorder.OutputFormat.THREE_GPP};
+    private String file_exts[] = {AUDIO_RECORDER_FILE_EXT_MP3, AUDIO_RECORDER_FILE_EXT_3GP};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity3);
+        buttonstart = (Button) findViewById(R.id.butstart);
+        buttonstop = (Button) findViewById(R.id.butstop);
 
-        tts = new TextToSpeech(this, this);
-    }
+        Preview = (Button) findViewById(R.id.review);
 
-    @Override
-    public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS) {
+        buttonstart.setOnClickListener(new View.OnClickListener() {
 
-            int result = tts.setLanguage(Locale.ENGLISH);
+            public void onClick(View v) {
 
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
-            {
-                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                    Toast.makeText(this, "This Language is not supported", Toast.LENGTH_LONG).show();
-
-                }
-
-                else {
-                    Toast.makeText(this, "Ready to Speak", Toast.LENGTH_LONG).show();
-
-                }
+                startRecording();
 
             }
+        });
 
+        buttonstop.setOnClickListener(new View.OnClickListener() {
 
-            else {
-                Toast.makeText(this, "Can Not Speak", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+
+                stopRecording();
             }
+        });
 
-        }
 
+        Preview.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setAction(android.content.Intent.ACTION_VIEW);
+                File file = new File("/sdcard/test.mp3");
+                intent.setDataAndType(Uri.fromFile(file), "audio/*");
+                startActivity(intent);
+            }
+        });
 
     }
-    @Override
-    public void onDestroy()
-    {
-        // Do Not forget to Stop the TTS Engine when you do not require it
-        if (tts != null)
-        {
-            tts.stop();
-            tts.shutdown();
+
+
+
+    private String getFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, AUDIO_RECORDER_FOLDER);
+
+        if (!file.exists()) {
+            file.mkdirs();
         }
-        super.onDestroy();
+
+        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(output_formats[currentFormat]);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(getFilename());
+
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
+    private void stopRecording() {
+        if (null != recorder) {
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+
+            recorder = null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -77,35 +122,6 @@ public class MainActivity3 extends Activity implements TextToSpeech.OnInitListen
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
-
-    private void speakTheText() {
-
-
-        String textToSpeak = "This is your online Statement of purpose, you have the floor for 90seconds!";
-
-
-
-        tts.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null);
-
-
-        while(tts.isSpeaking()==true);
-
-
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-        try {
-            startActivityForResult(intent, RESULT_SPEECH);
-
-        } catch (ActivityNotFoundException e) {
-            Toast t = Toast.makeText(getApplicationContext(), "Oops major fail", Toast.LENGTH_SHORT);
-            t.show();
-
-        }
-
-    }
-
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -118,5 +134,4 @@ public class MainActivity3 extends Activity implements TextToSpeech.OnInitListen
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
